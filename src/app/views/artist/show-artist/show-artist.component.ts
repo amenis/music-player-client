62b3cf0d596509b0ad8bc190a3bc2489348b2eff
Fabian <Fabian>
2019-03-04 16:08:55 -0600

    'show artist data'

---
+++ src/app/views/artist/show-artist/show-artist.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap} from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { ArtistService } from '../../../services/artist.service';
+import { UserService } from '../../../services/user.service';
+import { Artist } from 'src/app/models/artist';
+import { Album } from 'src/app/models/album';
+
+@Component({
+  selector: 'app-show-artist',
+  templateUrl: './show-artist.component.html',
+  styleUrls: ['./show-artist.component.css']
+})
+export class ShowArtistComponent implements OnInit {
+
+  private token: any;
+  public artist: Artist;
+  public albums: Album[];
+  private id: string;
+  private url: string;
+  public messageAlbum: string;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private _artistService: ArtistService,
+              private _userService: UserService
+              ) {
+    this.token = _userService.getToken();
+    this.url = _userService.url;
+
+    this.route.params.subscribe(
+      params => {
+        this.id = params['id'];
+        this._artistService.getArtist(this.token, this.id).subscribe(
+          response => {
+             this.artist = response.artist;
+            }
+        );
+      }
+    );
+  }
+
+  getAlbumsList() {
+    this._artistService.getArtistAlbums(this.token, this.id).subscribe(
+      albumsList =>  this.albums = albumsList.albums
+    );
+  }
+
+  ngOnInit() {
+    this.getAlbumsList();
+  }
+
+}
