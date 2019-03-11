3fce7bafc2df29b45592aad93de4371c2649e7d0
Fabian <Fabian>
2019-03-11 13:41:23 -0600

    added the funtion to upload a imagen

--- src/app/views/artist/edit-artist/edit-artist.component.ts
+++ src/app/views/artist/edit-artist/edit-artist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtistService } from 'src/app/services/artist.service';
+import { UserService } from 'src/app/services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Artist } from 'src/app/models/artist';
 
 @Component({
   selector: 'app-edit-artist',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditArtistComponent implements OnInit {
 
-  constructor() { }
+  private id: string;
+  private artist: Artist;
+  private url: string;
+  private token: string;
+  public fileToUpload: Array<File>;
+
+  constructor(
+      private _artistService: ArtistService,
+      private _userService: UserService,
+      private _router: Router,
+      private _activatedRoute: ActivatedRoute
+    ) {
+      this.token = this._userService.getToken();
+      this.url = _userService.url;
+      this._activatedRoute.params.subscribe(
+        params => {
+          this.id = params['id'];
+          this._artistService.getArtist(this.token, this.id).subscribe(
+            response =>  {
+              this.artist = response.artist;
+              console.log( this.artist );
+            }
+          );
+        }
+       );
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this._artistService.artistEdit(this.id, this.artist).subscribe(
+      artistEdit => {
+        if ( this.fileToUpload != null ) {
+          this._artistService.makeFileRequest(this.id, [], this.fileToUpload)
+          .then( (result: any) => console.log(result)  );
+
+        } else {
+          console.log(artistEdit);
+        }
+      }
+    );
+  }
+
+  fileChangeEvent(fileInput: any) {
+    this.fileToUpload = <Array <File> >fileInput.target.files;
   }
 
 }
