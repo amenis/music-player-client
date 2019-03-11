b79992f1c06adf1247f9840c715fca6448058aa2
Fabian <Fabian>
2019-03-11 13:49:07 -0600

    added the paginate options

--- src/app/views/artist/artist/artist.component.ts
+++ src/app/views/artist/artist/artist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute, Params } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Config } from '../../../services/config';
 import { UserService } from '../../../services/user.service';
 import { Artist } from '../../../models/artist';
@@ -16,30 +16,67 @@ export class ArtistComponent implements OnInit {
 
   public titulo: string;
   public artist: Artist[];
-  public token;
-  public identity;
+  public token: any;
+  public identity: any;
   public url: string;
+  public next_page: number;
+  public prev_page: number;
 
   constructor(
     private _artistService: ArtistService,
     private _userService: UserService,
+    private _router: Router,
     private _activatedRoute: ActivatedRoute
   ) {
     this.url = Config.url;
-    this.showListArtist();
+    this.token = this._userService.getToken();
+    this.next_page = 1;
+    this.prev_page = 1;
    }
 
   ngOnInit() {
     this.identity = this._userService.getIdentity();
     this.titulo = 'Artist';
+    this.showListArtist();
   }
 
   showListArtist() {
-    this._artistService.getArtistList().subscribe(
-      response => {
-        this.artist = response.artist;
-        console.log(this.artist);
-      }
-    );
+
+      this._activatedRoute.params.forEach(
+        (params: Params) => {
+          let page = +params['page'];
+          if (!page) {
+            page = 1;
+          } else {
+            this.next_page = page + 1;
+            this.prev_page = page - 1;
+            if (this.prev_page === 0) {
+              this. prev_page = 1;
+            }
+          }
+
+          this._artistService.getArtistList( this.token, page  ).subscribe(
+            artistList => {
+              if ( !artistList.artist ) {
+                this._router.navigate(['/']);
+              } else {
+                this.artist = artistList.artist;
+              }
+            },
+            error => {
+               const errorMessge = <any>error;
+               if ( errorMessge != null ) {
+                 const body = JSON.parse(error._body);
+                 console.log(error);
+               }
+            }
+          );
+
+        }
+      );
+  }
+
+  editArtist(token, idArtist) {
+
   }
 }
