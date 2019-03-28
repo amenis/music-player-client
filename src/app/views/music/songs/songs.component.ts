0e034a3aa1c5e34d279d697939b3d93bb07105e4
Fabian <Fabian>
2019-03-28 13:42:24 -0600

    song view

--- src/app/views/music/songs/songs.component.ts
+++ src/app/views/music/songs/songs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Song } from 'src/app/models/song';
+import { SongsService } from 'src/app/services/songs.service';
+import { Config } from 'src/app/services/config';
 
 @Component({
   selector: 'app-songs',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongsComponent implements OnInit {
 
-  constructor() { }
+  private songs: Song[] ;
+  private url;
+
+  constructor(private songService: SongsService) {
+    this.url = Config.url;
+  }
 
   ngOnInit() {
+    this.songService.getAllSongs().subscribe(
+      songs => {
+        this.songs = songs.songs;
+      }
+    );
+  }
+
+  play(file) {
+    document.getElementById('mp3-source').setAttribute('src', `${this.url}getSongFile/${file}`);
+    const media = document.getElementById('player');
+    (media as any).load();
+    (media as any).play();
   }
 
 }
